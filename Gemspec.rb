d81ae3f7614bddc63893955ee0fafe07e4f5960d
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-05-11 18:06:25 -0700

    Gemspec.rb [V1]
    Version 1 of the Gemspec file for this project.

---
+++ Gemspec.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/ruby
+# Start of script
+# Gemspec configuration and bundling
+# Gems are sorted in A-Z order
+# Misc gems
+gem: "/SPM2001_LifeArchive_Images_GitHub.gemspec"
+# Linux Gems
+# None yet
+# Windows Gems
+# None yet
+# BSD/MacOS Gems
+# None yet
+# File info
+# File type: Ruby Gemspec file (*.gemspec)
+# File version: 1 (2022, Tuesday, May 3rd at 6:51 pm PST)
+# Line count (including blank lines and compiler line): 18
+# End of script
